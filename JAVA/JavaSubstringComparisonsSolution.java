93b71e2243605b7f432c17f6edf6e5485af1978a
cristianbambague <cristian.bambague@horisoft.co>
2022-06-06 13:42:24 -0500

    add explication solution

--- JAVA/JavaSubstringComparisonsSolution.java
+++ JAVA/JavaSubstringComparisonsSolution.java
@@ -7,8 +7,8 @@ public class JavaSubstringComparisonsSolution {
         String s = "welcometojava";// scan.next();
         int k = 3; // scan.nextInt();
         // scan.close();
-
-        System.out.println(getSmallestAndLargest(s, k));
+        //compareTo();
+       System.out.println(getSmallestAndLargest(s, k));
     }
 
     public static String getSmallestAndLargest(String s, int k) {
@@ -17,17 +17,35 @@ public class JavaSubstringComparisonsSolution {
         
         for(int i = 0; i <= s.length() - k; i++){
             String sTemp = s.substring(i, i + k);
-            System.out.println(sTemp + "?" + smallest);
+            System.out.println(sTemp.compareTo(smallest));
             if(sTemp.compareTo(smallest) < 0) {
                 smallest = sTemp;
             }
-            System.out.println(sTemp + "?" + largest);
+            System.out.println(sTemp.compareTo(largest));
             if(sTemp.compareTo(largest) > 0) {
                 largest = sTemp;
             } 
         }
         
         return smallest + "\n" + largest;
+    }
+
+    public static void compareTo(){
+        /*
+         * va retornar < 0 si la cadena es lexicográficamente menor que la otra cadena
+         */
+        String str_Sample = "a";
+        System.out.println("Compare To 'a' b is : " + str_Sample.compareTo("b"));
+        /*
+         * va retornar > 0 si la cadena es lexicográficamente mayor que la otra cadena 
+         */
+        str_Sample = "b";
+        System.out.println("Compare To 'b' a is : " + str_Sample.compareTo("a"));
+        /*
+         * va retornar 0 si la cadena es igual a la otra cadena.
+         */
+        str_Sample = "b";
+        System.out.println("Compare To 'b' b is : " + str_Sample.compareTo("b"));
     }
 
 
