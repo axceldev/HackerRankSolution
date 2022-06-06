78b2989eac49be1d5f79d3c231b1a5f69560e562
cristianbambague <cristian.bambague@horisoft.co>
2022-06-06 16:59:29 -0500

    new execersice

---
+++ JAVA/JavaAnagramsSolution.java
@@ -0,0 +1,14 @@
+public class JavaAnagramsSolution {
+    public static void main(String[] args) {
+        String a = "";
+        String b = "";
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+    public static boolean isAnagram(String a, String b) {
+        boolean isAnagram = true;
+        
+
+        return isAnagram;
+    }
+}
