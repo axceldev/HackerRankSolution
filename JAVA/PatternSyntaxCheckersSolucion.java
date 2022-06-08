0eab2be4e75d660f49ede016501af896ff704422
cristianbambague <cristian.bambague@horisoft.co>
2022-06-08 00:18:23 -0500

    understand use exceptions

---
+++ JAVA/PatternSyntaxCheckersSolucion.java
@@ -0,0 +1,16 @@
+import java.util.regex.*;
+public class PatternSyntaxCheckersSolucion {
+    public static void main(String[] args) {
+        int testCases = 3;
+		while(testCases>0){
+			String pattern = "batcatpat(nat";
+              try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+              } catch (PatternSyntaxException e) { //cuando salga exception en la consola la mejor opcion para gestiona es try catch.
+                  System.out.println("Invalid");
+              }
+            testCases--;
+		}   
+    }
+}
